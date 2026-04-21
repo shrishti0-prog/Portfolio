1bd497fa69beaf3ccb00dcff8d6ef4cba27693ba
shrishti <shrishtigautam01@gmail.com>
2026-04-22 01:41:21 +0530

    Change form submission URL to production
    Updated the form submission URL to the production server.

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ form.addEventListener("submit", async (e) => {
   };
 
   try {
-    const res = await fetch("http://localhost:5000/send", {
+    const res = await fetch("https://portfolio-production-ae93.up.railway.app/send", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
