7d6e5498cab1e85eaa3157d097a0b6e82fd52a6d
shrishti <shrishtigautam01@gmail.com>
2026-04-22 21:18:38 +0530

    Update server response and maintain existing functionality

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.send("Backend is running 🚀");
 });
 // Route
 app.post("/send", async (req, res) => {
